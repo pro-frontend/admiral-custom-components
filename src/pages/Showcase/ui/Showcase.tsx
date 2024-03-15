2703d734f786e30688e2f00c97f8ad39ba9b8947
Dzmitryj Ivchenkov <dzmitryj.ivchenkov@gmail.com>
2024-03-15 18:01:10 +0300

    feat: add svg support, add alias "@/" as "src/"

--- src/pages/Showcase/ui/Showcase.tsx
+++ src/pages/Showcase/ui/Showcase.tsx
@@ -1,11 +1,13 @@
-import { LoadOnScrollTemplate } from "../../../features/LoadOnScrollTemplate";
-import { Container } from "../../../shared/ui";
+import { LoadOnScrollTemplate } from "@/features/LoadOnScrollTemplate";
+import { Container } from "@/shared/ui";
+import SvgChevronDownOutline from "@/shared/sources/svgChevronDownOutline.svg?react";
 
 export const Showcase = () => {
 	return (
 		<Container>
 			<p>Main page!</p>
 			<LoadOnScrollTemplate />
+			<SvgChevronDownOutline />
 		</Container>
 	);
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,21 @@
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [
+		react(),
+		svgr({
+			svgrOptions: {},
+		}),
+	],
+	resolve: {
+		alias: {
+			"@": resolve(__dirname, "src"),
+		},
+	},
 	server: {
 		open: true,
 	},
