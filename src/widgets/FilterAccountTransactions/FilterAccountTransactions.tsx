dc3082a14c9e315f8febbd88aa74d1df0621509b
Dzmitryj Ivchenkov <dzmitryj.ivchenkov@gmail.com>
2024-03-26 11:40:15 +0300

    feat: add SelectOneAsync to layout

--- src/widgets/FilterAccountTransactions/FilterAccountTransactions.tsx
+++ src/widgets/FilterAccountTransactions/FilterAccountTransactions.tsx
@@ -1,23 +1,10 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import type { ChangeEvent } from "react";
 import { Modal } from "@/features/Modal";
 import { Range, SelectOne } from "@/features/FormElements";
 import { Separator } from "@/shared/ui";
-import { useQuery } from "@tanstack/react-query";
-import { SelectAsync } from "@/features/FormElements/SelectAsync";
-
-async function searchPeopleByName(name: string) {
-	const response = await fetch(
-		"https://swapi.tech/api/people/?" +
-		new URLSearchParams({
-			search: name,
-		}),
-	);
-	if (!response.ok) {
-		throw new Error("Network response was not ok");
-	}
-	return response.json();
-}
+import { SelectOneAsync } from "@/features/FormElements/ui/SelectOneAsync";
+import { searchPeopleByName } from "@/entities/MockData";
 
 export const FilterAccountTransactions = () => {
 	const [rangeValue, setRangeValue] = useState<[number, number]>([0, 20]);
@@ -26,12 +13,16 @@ export const FilterAccountTransactions = () => {
 	const handleTaskTypeChange = (e: ChangeEvent<HTMLSelectElement>) => {
 		setTaskTypeValue(e.target.value);
 	};
+
 	return <Modal title="Фильтры модуля расчетных операций" buttonTitle="+">
 		<Separator />
-		<SelectAsync />
+		<SelectOneAsync
+			label="Тип таска:"
+			request={searchPeopleByName}
+		/>
 		<Separator />
 		<SelectOne
-			label="Тип таска:"
+			label="Филиал:"
 			value={taskTypeValue}
 			onChange={handleTaskTypeChange}
 			// data={[]}
