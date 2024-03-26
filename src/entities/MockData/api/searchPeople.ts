da73ee5354b01422b7b150e5301921cd81b4f382
Dzmitryj Ivchenkov <dzmitryj.ivchenkov@gmail.com>
2024-03-26 11:26:47 +0300

    feat: type api

--- src/entities/MockData/api/searchPeople.ts
+++ src/entities/MockData/api/searchPeople.ts
@@ -1,7 +1,9 @@
+import type { People } from "@/entities/MockData/People";
+
 export async function searchPeopleByName(
 	name: string,
 	page: string = "1",
-	limit: string = "10") {
+	limit: string = "10"): Promise<{ results: People }> {
 	const response = await fetch(
 		"https://swapi.tech/api/people?" +
 		new URLSearchParams({
